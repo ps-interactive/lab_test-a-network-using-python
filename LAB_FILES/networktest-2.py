d4d6f2c4fd02b498f54bce24a25b7dc53f794995
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-12-08 14:34:18 +0000

    Update networktest-2.py

--- LAB_FILES/networktest-2.py
+++ LAB_FILES/networktest-2.py
@@ -4,5 +4,3 @@ print('Perform custom port scan')
 # reference command: sudo nmap -sS -v -v -Pn 172.31.37.140
 dumpprocess = subprocess.Popen(['sudo','nmap','-sS','-v','-v','-Pn','add.ip.address.here'])
 print(str(dumpprocess.stdout))
-
-# , str(dumpprocess.pid)
