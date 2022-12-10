5d40d45c378ee030a52530e7473f7bc509cdcb55
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-12-10 11:39:06 +0000

    Update networktest-3.py

--- LAB_FILES/networktest-3.py
+++ LAB_FILES/networktest-3.py
@@ -1,5 +1,8 @@
 import subprocess
 
 print('Perform regular ping, 3 times')
-pingprocess = subprocess.Popen(['ping','-c','3','add.ip.address.here'])
-print(str(pingprocess.stdout))
+pingprocess1 = subprocess.Popen(['ping','-c','3','add.ip.address.here'])
+print(str(pingprocess1.stdout))
+
+pingprocess2 = subprocess.Popen(['ping','-c','3','add.ip.address.here'])
+print(str(pingprocess2.stdout))
