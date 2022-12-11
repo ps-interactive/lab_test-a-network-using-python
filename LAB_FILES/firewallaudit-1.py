45eb5743887ecbf0d199534d729941528ada6b55
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-12-11 01:28:49 +0000

    Update firewallaudit-1.py

--- LAB_FILES/firewallaudit-1.py
+++ LAB_FILES/firewallaudit-1.py
@@ -1,24 +1,24 @@
 import subprocess
 
-print('Perform audit of Linux firewall')
+print('Perform Linux firewall audit')
 fwprocess = subprocess.run(['sudo','ufw','status'], stdout=subprocess.PIPE)
 results = str(fwprocess.stdout)
 
-# Check port are compliance with company policy
+# Check port compliance with company policy
 bannedports = ['20','21','80','110','4444']
 for item in bannedports:
     if item in results:
-        print("port "+item+" found and not permitted by the company")
+        print("port "+item+" found, not permitted by the company")
 
 # Check for IPv6, company does not use this protocol
 if "(v6)" in results:
-    print("IPv6 rules identified, recommend remove IPv6 network if unused")
+    print("IPv6 rules identified, recommend remove IPv6 network")
 
 # Count rules
 denyrulescount = results.count('DENY')
 if denyrulescount == 0:
-    print("No DENY rules found!")
+    print("No DENY rules found!!!")
 else:
     print(denyrulescount+" DENY rules found for review")
 
-print(result.count("Anywhere")+" Anywhere rules identified")
+print(result.count("Anywhere")+" Anywhere rules identified for review")
